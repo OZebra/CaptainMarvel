b319c767b0f45704eb86ed1f4de98aa877ca10ae
Jose Carlos Carvalho Brandao <jccb@WINDOWS.CIN.UFPE.BR>
2019-11-29 13:26:43 -0300

    Now filtering

--- testesFourrier.m
+++ testesFourrier.m
@@ -9,8 +9,8 @@ data_size = length(data);
 t = 0:1/Fs:data_size/Fs-1/Fs;
 subplot(2,3,1);plot(t,data);title('Data x t');xlabel('Tempo');ylabel('Amplitude');
 
-noise = 1e-1*sin(2*pi*523.25*t);
-data_noise = data + noise';
+noise = 1e-2*sin(2*pi*523.25*t);
+data = data + noise';
 
 furData = fft(data)/data_size;
 furFreq = (0:length(furData)-1)*Fs/length(furData);
@@ -23,11 +23,13 @@ subplot(2,3,3);plot(fShift, abs(furDataShift));title('Fourrier Data Shifted x w'
 begFreq = 522.25 / (Fs/2);
 endFreq = 524.25 / (Fs/2);%Normalizando frequencias para a funcao butter;
 
-[b,a] = butter(10, [begFreq, endFreq], 'stop');
-
+[b,a] = butter(2, [begFreq, endFreq], 'stop');
 filtered_data = filter(b,a,data);
+subplot(2,3,4);plot(t, filtered_data);title('Filtered data');
 
-
+ffurData = fft(filtered_data)/length(filtered_data);
+ffurFreq = (0:length(ffurData)-1)*Fs/length(ffurData);
+subplot(2,3,5);plot(ffurFreq, abs(ffurData));title('Filtered fourrier');
 
 player_normal = audioplayer(data, Fs);
 player_noise = audioplayer(data_noise, Fs);
